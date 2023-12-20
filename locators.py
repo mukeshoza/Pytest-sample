80bd668dfcd1f38bd7d0ec977115b20849e7d11b
mukesh <mukesh@uxcam.com>
2023-12-20 12:25:40 +0545

    assesment

---
+++ locators.py
@@ -0,0 +1,45 @@
+# id
+username = "user-name"
+password = "password"
+submit = "login-button"
+product_page = "title"
+add_to_cart = "add-to-cart-sauce-labs-backpack"
+add_to_cart_1 = "add-to-cart-sauce-labs-bike-light"
+username_value_locked = 'locked_out_user'
+checkout = 'shopping_cart_container'
+checkout_first_name = 'first-name'
+checkout_last_name = 'last-name'
+checkout_zip = 'postal-code'
+checkout_continue = 'continue'
+summary_box = 'summary_info'
+
+
+# x-path
+check_page = '//div[@class="login_logo"]'
+all_items = '//*[@class="inventory_list"]//*[@class="inventory_item_name"]'
+filter_z_a = '//select[@class="product_sort_container"]/option[2]'
+low_to_high = '//select[@class="product_sort_container"]/option[3]'
+high_to_low = '//select[@class="product_sort_container"]/option[4]'
+checkout_button = "//button[@id='checkout']"
+checkout_finish = "//button[@id='finish']"
+checkout_message = '//h2[@class="complete-header"]'
+
+# css_selector
+all_listed_items = '.inventory_item_name'
+error_locked_user = "h3[data-test='error']"
+
+# class_name
+cart_value = "shopping_cart_badge"
+select_filter = "product_sort_container"
+cart_item = "cart_item_label"
+
+
+# input_values
+username_value = 'standard_user'
+password_value = 'secret_sauce'
+locked_user = 'locked_out_user'
+first_name = "Mukesh"
+last_name = "Ojha"
+zip_code = "44600"
+
+
